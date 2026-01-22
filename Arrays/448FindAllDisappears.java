9c20f379eb503423e7735aa47704ff4463017769
Aniket Shukla <152735267+theRoutines@users.noreply.github.com>
2026-01-22 16:38:16 +0530

    Implement method to find all disappeared numbers

---
+++ Arrays/448FindAllDisappears.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+
+        List<Integer> result = new ArrayList<>();
+
+        // mark visited numbers
+        for (int i = 0; i < nums.length; i++) {
+            int index = Math.abs(nums[i]) - 1;
+
+            if (nums[index] > 0) {
+                nums[index] = -nums[index];
+            }
+        }
+
+        // collect missing numbers
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                result.add(i + 1);
+            }
+        }
+
+        return result;
+    }
+}
